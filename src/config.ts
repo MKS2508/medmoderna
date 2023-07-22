7ddb8abe0cf260be182719e35320773e021fbfb7
Marcos Asensio <60777608+MKS2508@users.noreply.github.com>
2023-07-22 20:38:47 +0200

    Update config.ts

--- src/config.ts
+++ src/config.ts
@@ -1,5 +1,5 @@
 export const REACT_APP_API_KEY= "KEY";
-export const PROD_URL = "https://93.93.118.47/api";
+export const PROD_URL = "http://93.93.118.47/api";
 export const INT_URL  = "https://medmoderna-api.vercel.app/api";
 export const TESTING_URL = "http://localhost:8080/api";
 export const API_URL = PROD_URL;
