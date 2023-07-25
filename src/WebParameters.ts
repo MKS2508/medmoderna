5fa8a4f8b9f89a4e1850628f80d70f390e916732
Marcos Asensio <dam02.2020.jesuitas@gmail.com>
2023-07-25 19:26:26 +0200

    Update WebParameters.ts

--- src/WebParameters.ts
+++ src/WebParameters.ts
@@ -3,10 +3,10 @@ import {FaBong, FaCannabis, FaCapsules, FaHandHoldingWater, FaJoint, FaLightbulb
 export const FACEBOOK_LINK_1= "https://www.facebook.com/110763457854490/photos/a.129307232666779/347631650834335/?type=3&theater";
 export const FACEBOOK_LINK_2= "https://www.facebook.com/plugins/post.php?href=https%3A%2F%2Fwww.facebook.com%2F110763457854490%2Fphotos%2Fa.129307232666779%2F347631650834335%2F%3Ftype%3D3%26theater&width=500&show_text=true&height=680&appId";
 
-export const INSTAGRAM_LINK_1= "https://www.instagram.com/p/Ckqxnp9DKZx/embed";
-export const INSTAGRAM_LINK_2= "https://www.instagram.com/p/COi_Ep9nW2A/embed";
-export const INSTAGRAM_LINK_3= "https://www.instagram.com/p/COi_Ep9nW2A/embed";
-export const INSTAGRAM_LINK_4= "https://www.instagram.com/p/COi_Ep9nW2A/embed";
+export const INSTAGRAM_LINK_1= "https://www.instagram.com/p/Cd8uyvXswLL/embed";
+export const INSTAGRAM_LINK_2= "https://www.instagram.com/p/Cd8uyvXswLL/embed";
+export const INSTAGRAM_LINK_3= "https://www.instagram.com/p/Cd8uyvXswLL/embed";
+export const INSTAGRAM_LINK_4=  "https://www.instagram.com/p/Cd8uyvXswLL/embed";
 
 export const VIDEO_LINK_1= "https://medmoderna.b-cdn.net/videohome3.mp4";
 export const VIDEO_POSTER_1= posterGifVideo1;
@@ -29,7 +29,7 @@ export const IMAGEN_CARRUSEL_1 = fondo;
 export const IMAGEN_CARRUSEL_2 = fondo2;
 export const IMAGEN_CARRUSEL_3 = fondo3;
 
-export const TEXTO_DESCRIPTIVO= "En Medicina Moderna Grow Shop encontrarás una amplia selección de productos para la cultura y el crecimiento de plantas, así como todas las herramientas que necesitas.";
+export const TEXTO_DESCRIPTIVO= "";
 
 export const CATEGORIES = [
     { name: "CBD", link: "/cbd", icon: FaCapsules },
